cb256aed426d572ea053a9445c4e736ea2f3db06
Davidslc <djjohnson@gmail.com>
2015-03-31 18:32:35 -0600

    x

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,6 +16,8 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
+if (isset($_POST['submit']))
+{
 $to = 'djjohnsonslc@gmail.com'; // Add your email address in-between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
@@ -23,4 +25,5 @@ $mail_status = mail($to, $email_subject, $email_body);
 ?>
 <p>Success! Body: <?php echo $email_body ?> Status: <?php echo $mail_status ?> </p>
 <?php
+}
 ?>
